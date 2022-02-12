1bac004e00ab6089df3bacea46a9e113106de4ca
boshea5 <bernardoshea@mac.com>
2022-02-12 14:16:32 +0000

    changed layout to html radio buttons - add javascript to print results to browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log ("test")
+function result()
+{
+	var score=0;
+	if(document.getElementById('correct1').checked)
+		{	score++;
+		}
+	if(document.getElementById('correct2').checked)
+		{	score++;
+		}
+	if(document.getElementById('correct3').checked)
+		{	score++;
+		}
+ alert("Your score is:"+score);
+    }
