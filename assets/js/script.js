050c6b7f206d2186be2800bbbc1fda76ce061414
boshea5 <bernardoshea@mac.com>
2022-02-13 11:12:03 +0000

    add const variable  instead of var and moved function from the index.html to the .js file to use get element by instead

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,65 @@ function result()
 		}
  alert("Your score is:"+score);
     }
+
+	const Question = [{
+        id: 0,
+        q: " Q1. What is The Tallest Building in Ireland",
+        a: [{ text: "Liberty Hall", isCorrect: true },
+            { text: "The Cork Opera House", isCorrect: false },
+            { text: "Limerick Clarion", isCorrect: false },
+            { text: "Galway GPO", isCorrect: false }
+        ]
+  
+    },
+	Question = [{
+        id: 0,
+        q: " Q1. What is The Tallest Building in Ireland",
+        a: [{ text: "Liberty Hall", isCorrect: true },
+            { text: "The Cork Opera House", isCorrect: false },
+            { text: "Limerick Clarion", isCorrect: false },
+            { text: "Galway GPO", isCorrect: false }
+        ]
+  
+    },Question = [{
+        id: 0,
+        q: " Q1. What is The Tallest Building in Ireland",
+        a: [{ text: "Liberty Hall", isCorrect: true },
+            { text: "The Cork Opera House", isCorrect: false },
+            { text: "Limerick Clarion", isCorrect: false },
+            { text: "Galway GPO", isCorrect: false }
+        ]
+  
+    },
+	Question = [{
+        id: 0,
+        q: " Q1. What is The Tallest Building in Ireland",
+        a: [{ text: "Liberty Hall", isCorrect: true },
+            { text: "The Cork Opera House", isCorrect: false },
+            { text: "Limerick Clarion", isCorrect: false },
+            { text: "Galway GPO", isCorrect: false }
+        ]
+  
+    },
+	Question = [{
+        id: 0,
+        q: " Q1. What is The Tallest Building in Ireland",
+        a: [{ text: "Liberty Hall", isCorrect: true },
+            { text: "The Cork Opera House", isCorrect: false },
+            { text: "Limerick Clarion", isCorrect: false },
+            { text: "Galway GPO", isCorrect: false }
+        ]
+  
+    },
+	Question = [{
+        id: 0,
+        q: " Q1. What is The Tallest Building in Ireland",
+        a: [{ text: "Liberty Hall", isCorrect: true },
+            { text: "The Cork Opera House", isCorrect: false },
+            { text: "Limerick Clarion", isCorrect: false },
+            { text: "Galway GPO", isCorrect: false }
+        ]
+  
+    },
+
+  
