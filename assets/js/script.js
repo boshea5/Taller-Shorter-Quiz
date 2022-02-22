11743a17e63b61c550a18cea1c4e1fd86031e50f
boshea5 <bernardoshea@mac.com>
2022-02-22 12:20:10 +0000

    change html to move questions to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,4 @@
-function result()
-{
-	var score=0;
-	if(document.getElementById('correct1').checked)
-		{	score++;
-		}
-	if(document.getElementById('correct2').checked)
-		{	score++;
-		}
-	if(document.getElementById('correct3').checked)
-		{	score++;
-		}
- alert("Your score is:"+score);
-    }
+
 
 	const Question = [{
         id: 0,
@@ -51,26 +38,4 @@ function result()
             { text: "Galway GPO", isCorrect: false }
         ]
   
-    },
-	Question = [{
-        id: 0,
-        q: " Q1. What is The Tallest Building in Ireland",
-        a: [{ text: "Liberty Hall", isCorrect: true },
-            { text: "The Cork Opera House", isCorrect: false },
-            { text: "Limerick Clarion", isCorrect: false },
-            { text: "Galway GPO", isCorrect: false }
-        ]
-  
-    },
-	Question = [{
-        id: 0,
-        q: " Q1. What is The Tallest Building in Ireland",
-        a: [{ text: "Liberty Hall", isCorrect: true },
-            { text: "The Cork Opera House", isCorrect: false },
-            { text: "Limerick Clarion", isCorrect: false },
-            { text: "Galway GPO", isCorrect: false }
-        ]
-  
-    },
-
-  
+    }
