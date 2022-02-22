419eb0d9742efa85df0a1cf3b8684ea553479010
boshea5 <bernardoshea@mac.com>
2022-02-22 12:52:34 +0000

    redo answer box and 4 choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,40 +2,40 @@
 
 	const Question = [{
         id: 0,
-        q: " Q1. What is The Tallest Building in Ireland",
-        a: [{ text: "Liberty Hall", isCorrect: true },
-            { text: "The Cork Opera House", isCorrect: false },
-            { text: "Limerick Clarion", isCorrect: false },
-            { text: "Galway GPO", isCorrect: false }
+        q: " Q1. What is The Tallest Building in Dublin",
+        a: [{ text: "The Capital Dock Tower", isCorrect: true },
+            { text: "Liberty Hall", isCorrect: false },
+            { text: "The GPO", isCorrect: false },
+            { text: "The Spire", isCorrect: false }
         ]
   
     },
 	Question = [{
         id: 0,
-        q: " Q1. What is The Tallest Building in Ireland",
-        a: [{ text: "Liberty Hall", isCorrect: true },
+        q: " Q1. What is The Tallest Building in Cork",
+        a: [{ text: "The Elysian", isCorrect: true },
             { text: "The Cork Opera House", isCorrect: false },
-            { text: "Limerick Clarion", isCorrect: false },
-            { text: "Galway GPO", isCorrect: false }
+            { text: "Cork Cathedral", isCorrect: false },
+            { text: "Cork County Hall", isCorrect: false }
         ]
   
     },Question = [{
         id: 0,
-        q: " Q1. What is The Tallest Building in Ireland",
-        a: [{ text: "Liberty Hall", isCorrect: true },
-            { text: "The Cork Opera House", isCorrect: false },
-            { text: "Limerick Clarion", isCorrect: false },
-            { text: "Galway GPO", isCorrect: false }
+        q: " Q1. What County has the shortest coastline in Ireland",
+        a: [{ text: "Letrim", isCorrect: true },
+            { text: "Longford", isCorrect: false },
+            { text: "Limerick", isCorrect: false },
+            { text: "Laois", isCorrect: false }
         ]
   
     },
 	Question = [{
         id: 0,
-        q: " Q1. What is The Tallest Building in Ireland",
-        a: [{ text: "Liberty Hall", isCorrect: true },
-            { text: "The Cork Opera House", isCorrect: false },
-            { text: "Limerick Clarion", isCorrect: false },
-            { text: "Galway GPO", isCorrect: false }
+        q: " Q1. Where is The shortest street in Ireland",
+        a: [{ text: "Omagh", isCorrect: true },
+            { text: "Sligo", isCorrect: false },
+            { text: "Galway City", isCorrect: false },
+            { text: "Kilkenny", isCorrect: false }
         ]
   
     }
